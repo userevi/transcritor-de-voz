165f3054e2c230fad356f7672d7cb352e2f6ae9f
marcelo1811 <marcelo.m1811@gmail.com>
2025-10-03 23:20:16 -0300

    fix: add missing newline in providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,5 +3,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
+
   return <ChakraProvider>{children}</ChakraProvider>
 }
